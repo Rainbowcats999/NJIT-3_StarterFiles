9c91e775d5b312843b7d7201075a0f00cb501847
Rainbowcats999 <526erizov@frhsd.com>
2025-12-03 11:33:14 -0500

    Added in additiona; functions into the movies in javascript

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+           title: "IMDB + Michael's Top 8 Movies",
+           owner: "Michael Example",
+           github: "https://github.com/johndoe/doe-P3"
       }
     },
       methods: {
